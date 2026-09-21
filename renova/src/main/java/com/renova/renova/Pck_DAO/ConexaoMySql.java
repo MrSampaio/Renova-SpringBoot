25a274612f6720f56cf59a8a0287df0715b7b86f
Julio Sampaio <jusampa2@gmail.com>
2026-09-21 09:45:43 -0300

    feat: implements complete MySql connection file

---
+++ renova/src/main/java/com/renova/renova/Pck_DAO/ConexaoMySql.java
@@ -0,0 +1,54 @@
+package com.renova.renova.Pck_DAO;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoMySql {
+    private static String sDriver;
+    private String sServidor;
+    private String sBase;
+
+    private static String connStr;
+
+    static Connection conn;
+
+    private static final String URL = "jdbc:mysql://localhost:3306/renova";
+    public static final String login = "root";
+    public static final String senha = "11905972";
+
+    public ConexaoMySql(){
+        this.sDriver = "com.mysql.cj.jdbc.Driver";
+        this.sServidor = "localhost";
+        this.connStr = "jdbc:mysql://localhost:3306/renova";
+    }
+
+    public static Connection getConn(String login, String senha) throws SQLException {
+        conn = null;
+        try{
+            Class.forName(sDriver);
+            conn = DriverManager.getConnection(connStr, login, senha);
+            System.out.println("Conectado!!");
+            return conn;
+
+        } catch (ClassNotFoundException e){
+            String error = "Driver nao encontrado";
+            throw new SQLException(error, e);
+
+        } catch (SQLException e){
+            String error = "Erro ao obter conexao :" + e;
+            throw new SQLException(error, e);
+        }
+    }
+
+    public void desconectar(){
+        try{
+            if(conn != null){
+                conn.close();
+                System.out.println("Banco desconectado.");
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
